20bbd30fa5f2f4f2045584cf0cc9c71f89c96604
Michael Pope <map7@corsairsolutions.com.au>
2013-02-16 21:25:02 +1100

    Snake is displayed at random position in terrain

--- snake.js
+++ snake.js
@@ -20,38 +20,39 @@ function handler (req, res) {
 };
 
 function Snake(name) {
-  this.name = name;
-  this.char = this.name === null ? '#' : this.name[0];
-  this.move = function(pos) {
-    this.x = pos.x;
-    this.y = pos.y;
-  };
+	this.name = name;
+	this.char = this.name === null ? '#' : this.name[0];
+
+	this.move = function(pos) {
+		this.x = pos.x;
+		this.y = pos.y;
+	};
 };
 
 function Map() {
 	this.width = 80;
 	this.height = 40;
 
-  this.terrain = [];
-  for (i=0; i<this.height; i++) {
-    var row = [];
-    for (j=0; j<this.width; j++) {
-      row[j] = ".";
-    }
-    this.terrain[i] = row;
-  }
+	this.terrain = [];
+	for (i=0; i<this.height; i++) {
+		var row = [];
+		for (j=0; j<this.width; j++) {
+			row[j] = ".";
+		}
+		this.terrain[i] = row;
+	}
 
-  this.placeSnake = function(snake) {
-    var x = parseInt(Math.random() * this.width);
-    var y = parseInt(Math.random() * this.height);
+	this.placeSnake = function(snake) {
+		var x = parseInt(Math.random() * this.width);
+		var y = parseInt(Math.random() * this.height);
+		this.terrain[y][x] = "x"
+	};
 
-  };
-
-  this.toString = function() {
-    return _(this.terrain).map(function(row) {
-      return row.join("") + "\n";
-    }).join("");
-  };
+	this.toString = function() {
+		return _(this.terrain).map(function(row) {
+			return row.join("") + "\n";
+		}).join("");
+	};
 };
 
 var map = new Map();
@@ -61,26 +62,28 @@ io.sockets.on('connection', function (socket) {
 	socket.on('start', function(name){
 		console.log('in start');
 
-		// Setup player and attach to users scope through sockets.
+		// Setup snake and attach to users scope through sockets.
 		socket.snake = new Snake(name);
-    map.placeSnake(socket.snake);
+		map.placeSnake(socket.snake);
 
 		// Broadcast sends message to everyone but the new user.
-		socket.broadcast.emit('announce', socket.player.name + ' entered');
+		socket.broadcast.emit('announce', socket.snake.name + ' entered');
 	});
 	
 	socket.on('say', function(key) {
-    var movement = {
-      'left': {x: -1, y: 0},
-      'up':   {x: 0, y: -1},
-      'right':{x: 1, y: 0},
-      'down': {x: 0, y: 1}
-    }[key];
+		var movement = {
+			'left': {x: -1, y: 0},
+			'up':   {x: 0, y: -1},
+			'right':{x: 1, y: 0},
+			'down': {x: 0, y: 1}
+		}[key];
 
-    // Send shit to everyone but yourself.
-    // socket.broadcast.emit('announce', 'This is a broadcast'); 
+		// Send shit to everyone but yourself.
+		// socket.broadcast.emit('announce', 'This is a broadcast'); 
 
-    map.moveSnake(socket.player.charater, movement);
+		// Move the snake with the keyboard
+    map.placeSnake(this.snake);
+    // map.placeSnake(socket.snake.char, movement);
 
     io.sockets.emit('move', movement, map.toString());
 
