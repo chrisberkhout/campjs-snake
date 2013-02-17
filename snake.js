9292af255504ad71dfeebd6521effaead6432f97
Michael Pope <map7@corsairsolutions.com.au>
2013-02-17 21:05:28 +1100

    Fixed key refresh in browser

--- snake.js
+++ snake.js
@@ -13,6 +13,8 @@ module.exports = function(name) {
 	this.zombie = false;
 
 	this.invincible = true;
+	var that = this;
+	setInterval(function(){ that.invincible = false; }, 6000);
 
 	this.randomColor = function(){
 		colors=['#1f77b4','#ff7f0e','#2ca02c','#d62728','#9467bd','#8c564b','#e377c2','#7f7f7f','#bcbd22','#17becf']
