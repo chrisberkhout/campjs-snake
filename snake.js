1003db6ca0e282b5661b266f827741dfb1434584
Michael Pope <map7@corsairsolutions.com.au>
2013-02-17 15:09:36 +1100

    Format snake

--- snake.js
+++ snake.js
@@ -4,20 +4,20 @@ module.exports = function(name) {
 
 	this.name = name;
 	this.character = this.name === null ? '#' : this.name[0];
-  this.positions = [];
-  this.head = function() { return _(this.positions).last(); };
-  this.length = 1;
-  this.lastDirection = {
-    x: parseInt(Math.random() * 3) - 1,
-    y: parseInt(Math.random() * 3) - 1
-  };
+	this.positions = [];
+	this.head = function() { return _(this.positions).last(); };
+	this.length = 1;
+
+	this.lastDirection = {
+		x: parseInt(Math.random() * 3) - 1,
+		y: parseInt(Math.random() * 3) - 1
+	};
 
 	this.setPos = function(pos) {
 		this.x = pos.x;
 		this.y = pos.y;
-    this.positions.push(pos);
-    if (this.positions.length > this.length) { this.positions.shift(); }
+		this.positions.push(pos);
+		if (this.positions.length > this.length) { this.positions.shift(); }
 	};
-
 };
 
